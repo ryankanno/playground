8433716ad73350df144d555cf7aa67d196066b52
Ryan Kanno <ryankanno@localkinegrinds.com>
2011-11-06 16:04:02 -0500

    Updated gitignore Added fib.erl

--- erl/fib.erl
+++ erl/fib.erl
@@ -9,6 +9,6 @@ fib(N) when N > 0 ->
 
 %% Tail recursion
 fib2(N) -> fib2(N, 0, 1).
-fib2(0, Acc, _) -> Acc;
-fib2(N, Acc, Next) when N > 0 ->
-    fib2(N-1, Next, Acc+Next).
+fib2(0, Result, _) -> Result;
+fib2(N, Result, Next) when N > 0 ->
+    fib2(N-1, Next, Result+Next).
