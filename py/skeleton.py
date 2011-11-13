5aefb8afe5151e231af6878151c4c2d899f33b67
Ryan Kanno <ryankanno@localkinegrinds.com>
2011-11-13 08:16:51 -0500

    Added python skeleton wrapper

---
+++ py/skeleton.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import argparse
+import platform
+import logging
+import sys
+import os
+
+""" This module is just a template I use before performing magic """
+
+__all__     = ['main']
+__author__  = ""
+__url__     = ""
+__version__ = ""
+
+LOG_LEVEL = logging.DEBUG
+
+
+# Timing decorator
+def timing(func):
+    def wrapper(*args, **kwargs):
+        start  = time.clock() if 'Windows' == platform.system() else time.time()
+        retval = func(*args, **kwargs)
+        end    = time.clock() if 'Windows' == platform.system() else time.time()
+        print "%s took %0.3f ms" % (func.func_name, (end-start) * 1000.0)
+        return retval
+    return wrapper
+
+
+def init_argparser():
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument('--run-tests', action='store_true', help='run all tests')
+    return parser
+
+
+def do_work_son(args):
+    pass
+
+
+def main(argv=None):
+    logging.basicConfig(level=LOG_LEVEL, format='%(asctime)s %(levelname)s %(message)s')
+
+    if argv is None:
+        argv = sys.argv
+
+    parser = init_argparser()
+    args = parser.parse_args(argv)
+
+    try:
+        do_work_son(args)
+    except KeyboardInterrupt, e: 
+    except SystemExit, e: 
+    except Exception, e:
+        os._exit(1)
+
+    # Yayyy-yah
+    return 0
+
+
+def _test():
+    import doctest
+    doctest.testmod(sys.modules[__name__])
+
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv[1:]))
+
+# vim: filetype=python
