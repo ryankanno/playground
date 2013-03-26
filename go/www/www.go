18343e9b53f0b93548a0ae04979b6142aceb78f9
Ryan Kanno <ryankanno@localkinegrinds.com>
2013-03-26 07:51:51 -0400

    Updated www.go

--- go/www/www.go
+++ go/www/www.go
@@ -4,6 +4,7 @@ import (
     "net/http"
     "fmt"
     "log"
+    "os"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) { 
@@ -11,10 +12,18 @@ func handler(w http.ResponseWriter, r *http.Request) {
     fmt.Fprintf(w, "Hello world")
 }
 
+func Log(handler http.Handler) http.Handler {
+    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+        log.Printf("%s %s %s", r.RemoteAddr, r.Method, r.URL)
+        handler.ServeHTTP(w, r)
+    })
+}
+
 func main() {
+    log.SetOutput(os.Stdout)
+    port := ":9999"
     http.HandleFunc("/", handler)
-    err := http.ListenAndServe(":9999", nil)
-    if err != nil {
-        log.Printf("Error running hello world: %v", err)
-    }
+    if err := http.ListenAndServe(port, Log(http.DefaultServeMux)); err != nil { 
+		log.Printf("ListenAndServe Error: %v", err) 
+	}
 }
